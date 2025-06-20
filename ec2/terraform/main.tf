3a750dec5602fec0c94ec4279c6f5a300430182a
Roger Emerson <emerson.rogerjohn@gmail.com>
2025-06-20 01:26:31 -0500

    terraform files...

---
+++ ec2/terraform/main.tf
@@ -0,0 +1,61 @@
+provider "aws" {
+  region = "us-east-1" # or your desired region
+}
+
+# Step 1: Create a Security Group
+resource "aws_security_group" "launch_wizard_1" {
+  name        = "launch-wizard-1"
+  description = "launch-wizard-1 created 2025-06-20T06:05:53.592Z"
+  vpc_id      = "vpc-0dcfd380bf1ae6ff2"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "launch-wizard-1"
+  }
+}
+
+# Step 2: Create an EC2 Instance
+resource "aws_instance" "example" {
+  ami           = "ami-0f3f13f145e66a0a3"
+  instance_type = "t2.micro"
+
+  network_interface {
+    device_index         = 0
+    associate_public_ip_address = true
+    security_groups      = [aws_security_group.launch_wizard_1.id]
+  }
+
+  credit_specification {
+    cpu_credits = "standard"
+  }
+
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_put_response_hop_limit = 2
+    http_tokens                 = "required"
+  }
+
+  private_dns_name_options {
+    hostname_type                           = "ip-name"
+    enable_resource_name_dns_a_record       = true
+    enable_resource_name_dns_aaaa_record    = false
+  }
+
+  tags = {
+    Name = "example-instance"
+  }
+}
