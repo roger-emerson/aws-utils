86a49e93c983dbc75a3f6113eb1ea891d369f20a
Roger Emerson <emerson.rogerjohn@gmail.com>
2025-06-23 01:45:55 -0500

    initial efk application...

--- ec2/efk_terraform/main.tf
+++ ec2/efk_terraform/main.tf
@@ -1,1 +1,89 @@
-#init
+# EFK Stack Terraform Configuration
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "efk" {
+  name        = "efk-sg"
+  description = "Allow EFK components to communicate"
+  vpc_id      = "vpc-0786a3d5bc2ea7fc9"
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["10.100.100.0/28"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+locals {
+  ami_id = "ami-0c2b8ca1dad447f8a" # Amazon Linux 2 AMI in us-east-1
+}
+
+resource "aws_instance" "elasticsearch" {
+  ami                         = local.ami_id
+  instance_type               = "t3.small"
+  subnet_id                   = "subnet-0350629fcf0319671"
+  vpc_security_group_ids      = [aws_security_group.efk.id]
+  key_name                    = "terraform"
+  associate_public_ip_address = true
+  tags = {
+    Name = "elasticsearch.emersonlabs.net"
+  }
+  user_data = <<-EOF
+              #!/bin/bash
+              exec > >(tee /var/log/user_data.log|logger -t user-data -s 2>/dev/console) 2>&1
+              yum update -y
+              yum install docker -y
+              systemctl start docker
+              docker run -d --name elasticsearch -p 9200:9200 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:8.13.0
+              EOF
+}
+
+resource "aws_instance" "kibana" {
+  ami                         = local.ami_id
+  instance_type               = "t3.small"
+  subnet_id                   = "subnet-0350629fcf0319671"
+  vpc_security_group_ids      = [aws_security_group.efk.id]
+  key_name                    = "terraform"
+  associate_public_ip_address = true
+  tags = {
+    Name = "kibana.emersonlabs.net"
+  }
+  user_data = <<-EOF
+              #!/bin/bash
+              exec > >(tee /var/log/user_data.log|logger -t user-data -s 2>/dev/console) 2>&1
+              yum update -y
+              yum install docker -y
+              systemctl start docker
+              docker run -d --name kibana -p 5601:5601 -e ELASTICSEARCH_HOSTS=http://10.100.100.10:9200 docker.elastic.co/kibana/kibana:8.13.0
+              EOF
+}
+
+resource "aws_instance" "fluentd" {
+  ami                         = local.ami_id
+  instance_type               = "t3.small"
+  subnet_id                   = "subnet-0350629fcf0319671"
+  vpc_security_group_ids      = [aws_security_group.efk.id]
+  key_name                    = "terraform"
+  associate_public_ip_address = true
+  tags = {
+    Name = "fluentd.emersonlabs.net"
+  }
+  user_data = <<-EOF
+              #!/bin/bash
+              exec > >(tee /var/log/user_data.log|logger -t user-data -s 2>/dev/console) 2>&1
+              yum update -y
+              yum install docker -y
+              systemctl start docker
+              docker run -d --name fluentd -p 24224:24224 -p 24224:24224/udp fluent/fluentd:v1.16-1
+              EOF
+}
