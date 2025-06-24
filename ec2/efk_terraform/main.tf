5abe3332b6a3f83d4d46b9f514384baec8fbb0d8
Roger Emerson <emerson.rogerjohn@gmail.com>
2025-06-24 10:24:06 -0500

    change for action initiation

--- ec2/efk_terraform/main.tf
+++ ec2/efk_terraform/main.tf
@@ -104,7 +104,7 @@ resource "aws_instance" "kibana" {
   private_ip                  = "10.100.100.11"
   vpc_security_group_ids      = [aws_security_group.efk.id]
   key_name                    = "terraform"
-  associate_public_ip_address = false
+  associate_public_ip_address = true
   tags = {
     Name = "kibana.emersonlabs.net"
   }
