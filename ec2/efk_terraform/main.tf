38c5c95e5d32ce0258995d31b2d1653e86aad2a3
remerson <remerson@rocketsoftware.com>
2025-06-23 21:18:45 -0500

    removed s3 and db resources...

--- ec2/efk_terraform/main.tf
+++ ec2/efk_terraform/main.tf
@@ -13,45 +13,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "tf_state" {
-  bucket = "emersonlabs-terraform-state"
-
-  versioning {
-    enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
-  lifecycle {
-    prevent_destroy = true
-  }
-
-  tags = {
-    Name = "Terraform State Bucket"
-  }
-}
-
-resource "aws_dynamodb_table" "tf_locks" {
-  name         = "terraform-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name = "Terraform Lock Table"
-  }
-}
-
 resource "aws_security_group" "efk" {
   name        = "efk-sg"
   description = "Allow EFK components to communicate"
