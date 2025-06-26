d2a356a55b2c599a08afeffaaac0e76695ea2fc3
Roger Emerson <emerson.rogerjohn@gmail.com>
2025-06-25 23:20:03 -0400

    added s3 backend

--- modules/main.tf
+++ modules/main.tf
@@ -5,3 +5,12 @@ module "vpc1" {
   cidr        = "10.100.100.0/23"
   name_prefix = "Lab"
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "emersonlabs-terraform-state"
+    key            = "ec2/efk_terraform/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+  }
+}
