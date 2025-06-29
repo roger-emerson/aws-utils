334a7bc2423a6610897fbf8454546dd0c6b0f02e
remerson <remerson@rocketsoftware.com>
2025-06-29 00:52:25 -0500

    removed tfvars...

--- ec2/harvester/variables.tf
+++ ec2/harvester/variables.tf
@@ -1,4 +1,11 @@
 variable "my_ip_cidr" {
   description = "Your public IP address with CIDR mask (e.g. 203.0.113.1/32)"
   type        = string
+  default     = "0.0.0.0/0"
+}
+
+variable "key_pair" {
+  description = "ssh keypair for instances"
+  type        = string
+  default     = "terraform"
 }
